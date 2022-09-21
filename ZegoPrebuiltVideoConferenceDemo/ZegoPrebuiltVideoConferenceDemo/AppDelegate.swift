4b8794e31f59546ba3873d89c6c655cef2db03cf
nemoyuan <nemoyuan@zego.im>
2022-09-21 18:34:51 +0800

    init video conference demo

---
+++ ZegoPrebuiltVideoConferenceDemo/ZegoPrebuiltVideoConferenceDemo/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ZegoPrebuiltVideoConference
+//
+//  Created by zego on 2022/9/14.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ ZegoPrebuiltVideoConferenceDemo/ZegoPrebuiltVideoConferenceDemo/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  ZegoPrebuiltVideoConference
+//
+//  Created by zego on 2022/9/14.
+//
+
+import UIKit
+import ZegoUIKitSDK
+import ZegoUIKitPrebuiltVideoConference
+
+class ViewController: UIViewController {
+    
+    let appID: UInt32 = <#YourAppID#>
+    let appSign: String = <#YourAppSign#>
+    var userID: String = "\(arc4random())"
+    var userName: String?
+    
+    
+    @IBOutlet weak var conferenceIDTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func joinButtonClick(_ sender: Any) {
+        let config: ZegoUIKitPrebuiltVideoConferenceConfig = ZegoUIKitPrebuiltVideoConferenceConfig()
+        self.userName = String(format: "n_%@", self.userID)
+        guard let conferenceID = self.conferenceIDTextField.text else { return }
+        let videoConferenceVC: ZegoUIKitPrebuiltVideoConferenceVC = ZegoUIKitPrebuiltVideoConferenceVC.init(self.appID, appSign: self.appSign, userID: self.userID, userName: self.userName ?? "", conferenceID: conferenceID, config: config)
+        videoConferenceVC.modalPresentationStyle = .fullScreen
+        self.present(videoConferenceVC, animated: true, completion: nil)
+    }
+    
+
+}
+
